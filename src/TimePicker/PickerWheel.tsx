eb6aec6caf5d873af4efff7ed0c18989c9084f24
EgorDubovik <posik.web.m@gmail.com>
2024-07-24 08:53:42 -0500

    move all events to custom hook

--- src/TimePicker/PickerWheel.tsx
+++ src/TimePicker/PickerWheel.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import { getIndexTranslateY } from "./lib/utils";
 import { IPickerWheel } from "./types";
+import { useEventHandlers } from "./lib/hooks";
 
 const PickerWheel = (props: IPickerWheel) => {
 	const onGetItems = props.onGetItems;
@@ -55,120 +56,16 @@ const PickerWheel = (props: IPickerWheel) => {
       setItems(getItems(getValue()));
 	}, [indexTranslateY]);
 
-   const updateTranslateY = (newTranslateY:number) => {
-      if (!isLoop) {
-         if (newTranslateY < itemHeight * (itemsView - 1))
-            return itemHeight * (itemsView - 1);
-         if (newTranslateY > itemHeight * itemsView)
-            return itemHeight * itemsView;
-      }
-      return newTranslateY;
-   }
 
-	const startY = useRef(0);
-	const isDraging = useRef(false);
-   const hasDragged = useRef(false);
-   const draggDelta = useRef(0);
-   
-	const handleMouseDown = (e: any) => {
-      hasDragged.current = false;
-      draggDelta.current = 0;
-		isDraging.current = true;
-		startY.current = e.clientY;
-		if (itemsRef.current)
-			itemsRef.current.style.transition = "transform 0s ease-out";
-		document.addEventListener("mousemove", handleMouseMove);
-		document.addEventListener("mouseup", handleMouseUp);
-	};
-
-	const handleMouseMove = (e: any) => {
-		if (!isDraging.current) return;
-		const diff = e.clientY - startY.current;
-      draggDelta.current += diff;
-      if(draggDelta.current > 10 || draggDelta.current < -10) hasDragged.current = true;
-      setTranslateY((prev) => 
-         updateTranslateY(prev + diff)
-      );
-		startY.current = e.clientY;
-	};
-
-	const handleMouseUp = (e: any) => {
-		isDraging.current = false;
-      setTranslateY((prev) =>
-         updateTranslateY(Math.round(prev / itemHeight) * itemHeight)
-      );
-      
-		if (itemsRef.current)
-			itemsRef.current.style.transition = "transform 0.5s ease-out 0s";
-		document.removeEventListener("mousemove", handleMouseMove);
-		document.removeEventListener("mouseup", handleMouseUp);
-	};
-
-	const handleWheel = (e: any) => {
-		e.preventDefault();
-		if (e.deltaY < 0) {
-			setTranslateY((prev) => 
-            updateTranslateY(prev + itemHeight)
-         );
-         
-		} else {
-			setTranslateY((prev) => 
-            updateTranslateY(prev - itemHeight)
-         );
-		}
-	};
-
-   const handleClick = (ind: number) => {
-      if(hasDragged.current) return;
-      const diff = indexTranslateY - marginTopIndex - ind
-      setTranslateY((prev) => 
-         updateTranslateY(prev + diff * itemHeight)
-      );
-   }
-
-   // Touch Events
-	const handleTouchStart = (e: any) => {
-		isDraging.current = true;
-		startY.current = e.touches[0].clientY;
-		if (itemsRef.current) itemsRef.current.style.transition = 'transform 0s ease-out';
-		document.addEventListener('touchmove', handleTouchMove, { passive: false });
-		document.addEventListener('touchend', handleTouchEnd);
-	};
-
-	const handleTouchMove = (e: any) => {
-		if (!isDraging.current) return;
-		if (e.cancelable && typeof e.preventDefault === 'function') {
-			e.preventDefault(); // Prevent default scrolling behavior if possible
-		}
-		const diff = e.touches[0].clientY - startY.current;
-		setTranslateY((prev) => 
-         updateTranslateY(prev + diff)
-      );
-		startY.current = e.touches[0].clientY;
-	};
-
-	const handleTouchEnd = () => {
-		isDraging.current = false;
-		setTranslateY((prev) =>
-         updateTranslateY(Math.round(prev / itemHeight) * itemHeight)
-      );
-		if (itemsRef.current) itemsRef.current.style.transition = 'transform 0.5s ease-out';
-		document.removeEventListener('touchmove', handleTouchMove);
-		document.removeEventListener('touchend', handleTouchEnd);
-	};
-
-	useEffect(() => {
-		itemsRef.current?.addEventListener("wheel", handleWheel, {
-			passive: false,
-		});
-		return () => {
-			document.removeEventListener("mousemove", handleMouseMove);
-			document.removeEventListener("mouseup", handleMouseUp);
-         document.removeEventListener("touchmove", handleTouchMove);
-         document.removeEventListener("touchend", handleTouchEnd);
-		};
-	}, []);
-	
+	const { handleClick } =  useEventHandlers(
+		itemsRef,
+		itemHeight,
+		setTranslateY,
+		itemsView,
+		isLoop,
+		indexTranslateY,
+		marginTopIndex
+	);
 
 	return (
 		<div className="picker-wheel">
@@ -178,8 +75,6 @@ const PickerWheel = (props: IPickerWheel) => {
 			></div>
 			<div
 				className="picker-wheel-items"
-				onMouseDown={handleMouseDown}
-            onTouchStart={handleTouchStart}
 				ref={itemsRef}
 				style={{
 					transform: `translateY(${translateY}px)`,
--- src/TimePicker/lib/hooks.ts
+++ src/TimePicker/lib/hooks.ts
@@ -1,44 +1,126 @@
-import { useEffect, useState } from "react";
+import { useRef, useEffect } from "react";
 
-export function useItems({
-	initialDate,
-	onGetItems,
-	textFormat,
-	textItemStep,
-	itemHeight,
-	itemsView,
-}: {
-	initialDate: Date;
-	onGetItems: (date: Date, format: string, step: number) => any[];
-	textFormat: string;
-	textItemStep: number;
-	itemHeight: number;
-	itemsView: number;
-}) {
-	const [currentDate, setCurrentDate] = useState(initialDate);
-	const [items, setItems] = useState(() =>
-		onGetItems(initialDate, textFormat, textItemStep)
-	);
-	const [indexTranslateY, setIndexTranslateY] = useState(0);
+export function useEventHandlers(
+	itemsRef: React.MutableRefObject<HTMLDivElement | null>,
+	itemHeight: number,
+	setTranslateY: React.Dispatch<React.SetStateAction<number>>,
+	itemsView: number,
+	isLoop: boolean,
+   indexTranslateY: number,
+   marginTopIndex: number
+) {
+	const startY = useRef(0);
+	const isDraging = useRef(false);
+	const hasDragged = useRef(false);
+	const draggDelta = useRef(0);
+
+	const updateTranslateY = (newTranslateY: number) => {
+		if (!isLoop) {
+			if (newTranslateY < itemHeight * (itemsView - 1))
+				return itemHeight * (itemsView - 1);
+			if (newTranslateY > itemHeight * itemsView)
+				return itemHeight * itemsView;
+		}
+		return newTranslateY;
+	};
+
+	const handleMouseDown = (e: any) => {
+		hasDragged.current = false;
+		draggDelta.current = 0;
+		isDraging.current = true;
+		startY.current = e.clientY;
+		if (itemsRef.current)
+			itemsRef.current.style.transition = "transform 0s ease-out";
+		document.addEventListener("mousemove", handleMouseMove);
+		document.addEventListener("mouseup", handleMouseUp);
+	};
+
+	const handleMouseMove = (e: any) => {
+		if (!isDraging.current) return;
+		const diff = e.clientY - startY.current;
+		draggDelta.current += diff;
+		if (draggDelta.current > 10 || draggDelta.current < -10)
+			hasDragged.current = true;
+		setTranslateY((prev) => updateTranslateY(prev + diff));
+		startY.current = e.clientY;
+	};
+
+	const handleMouseUp = (e: any) => {
+		isDraging.current = false;
+		setTranslateY((prev) =>
+			updateTranslateY(Math.round(prev / itemHeight) * itemHeight)
+		);
+
+		if (itemsRef.current)
+			itemsRef.current.style.transition = "transform 0.5s ease-out 0s";
+		document.removeEventListener("mousemove", handleMouseMove);
+		document.removeEventListener("mouseup", handleMouseUp);
+	};
+
+	const handleWheel = (e: any) => {
+		e.preventDefault();
+		if (e.deltaY < 0) {
+			setTranslateY((prev) => updateTranslateY(prev + itemHeight));
+		} else {
+			setTranslateY((prev) => updateTranslateY(prev - itemHeight));
+		}
+	};
+
+   // Touch Events
+	const handleTouchStart = (e: any) => {
+		isDraging.current = true;
+		startY.current = e.touches[0].clientY;
+		if (itemsRef.current) itemsRef.current.style.transition = 'transform 0s ease-out';
+		document.addEventListener('touchmove', handleTouchMove, { passive: false });
+		document.addEventListener('touchend', handleTouchEnd);
+	};
+
+	const handleTouchMove = (e: any) => {
+		if (!isDraging.current) return;
+		if (e.cancelable && typeof e.preventDefault === 'function') {
+			e.preventDefault(); // Prevent default scrolling behavior if possible
+		}
+		const diff = e.touches[0].clientY - startY.current;
+		setTranslateY((prev) => 
+         updateTranslateY(prev + diff)
+      );
+		startY.current = e.touches[0].clientY;
+	};
+
+	const handleTouchEnd = () => {
+		isDraging.current = false;
+		setTranslateY((prev) =>
+         updateTranslateY(Math.round(prev / itemHeight) * itemHeight)
+      );
+		if (itemsRef.current) itemsRef.current.style.transition = 'transform 0.5s ease-out';
+		document.removeEventListener('touchmove', handleTouchMove);
+		document.removeEventListener('touchend', handleTouchEnd);
+	};
+
+    const handleClick = (ind: number) => {
+      if(hasDragged.current) return;
+      const diff = indexTranslateY - marginTopIndex - ind
+      setTranslateY((prev) => 
+         updateTranslateY(prev + diff * itemHeight)
+      );
+   }
 
 	useEffect(() => {
-		const newDate = new Date(initialDate);
-		setCurrentDate(newDate);
-		setItems(onGetItems(newDate, textFormat, textItemStep));
-	}, [initialDate]);
+		const wheel = itemsRef.current;
+		if (!wheel) return;
 
-	const updateItems = (translateY: number) => {
-		const newIndexTranslateY =
-			Math.round(translateY / itemHeight) * -1 + itemsView;
-		setIndexTranslateY(newIndexTranslateY);
-		setItems(onGetItems(currentDate, textFormat, textItemStep));
-	};
+		wheel.addEventListener("mousedown", handleMouseDown);
+      wheel.addEventListener("touchstart", handleTouchStart, { passive: false });
+		wheel.addEventListener("wheel", handleWheel, {
+			passive: false,
+		});
+		return () => {
+			document.removeEventListener("mousemove", handleMouseMove);
+			document.removeEventListener("mouseup", handleMouseUp);
+			document.removeEventListener("touchmove", handleTouchMove);
+			document.removeEventListener("touchend", handleTouchEnd);
+		};
+	}, [itemsRef]);
 
-	return {
-		currentDate,
-		items,
-		updateItems,
-		setIndexTranslateY,
-		indexTranslateY,
-	};
+   return { handleClick };
 }
